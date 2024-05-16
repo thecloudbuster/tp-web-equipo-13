8508873cfc9c00be0aa5768168b2a49a23e4c7f7
unknown <francocataldo7@gmail.com>
2024-05-16 17:18:04 -0300

    desarrolle parcialmente el foreach para que se muestren tantas tarjetas como articulos haya en la base

--- TPWeb_equipo-13/Carrito Web/Default.aspx.cs
+++ TPWeb_equipo-13/Carrito Web/Default.aspx.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using gestor;
 
 namespace Carrito_Web
 {
     public partial class _Default : Page
     {
+        public List<Articulo> ListaArticulo { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            gestionArticulo gestionArt = new gestionArticulo();
+            ListaArticulo = gestionArt.listar();
         }
     }
 }
